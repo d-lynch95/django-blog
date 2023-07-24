0858942e582019689b40da088e0229e6f190d2ca
d-lynch95 <darraghlynch1995@gmail.com>
2023-07-24 13:56:46 +0000

    update allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-ci.heroukapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-ci-04b566cc8f86.herokuapp.com', 'localhost']
 
 
 
