9d5c933016d3251e5ad213bc4bdd4a6601ebde33
d-lynch95 <darraghlynch1995@gmail.com>
2023-07-30 01:17:30 +0000

    update

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-ci-04b566cc8f86.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-ci-04b566cc8f86.herokuapp.com', 'localhost','8000-dlynch95-djangoblog-trewi9qz40l.ws-us102.gitpod.io']
 
 
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
@@ -137,7 +138,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL='/media'
+MEDIA_URL='/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
